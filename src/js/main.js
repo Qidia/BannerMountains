ed519f8a5d63c8bb52080e7926fc5f07973b2ad6
Qidia <qidian_n@mail.ru>
2024-10-28 00:23:02 +0300

    Added the logic of the slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 const imgMountains = document.querySelector(".imgMountains");
 const banner = document.querySelector(".banner");
+const slider = document.querySelector(".slider");
+let isDragging = false;
+let sliderPositionPercentage = 0;
 
 function getShiftImage() {
   imgMountains.classList.add("shiftImageMountains");
@@ -12,11 +15,47 @@ function getOriginalImage() {
 }
 
 banner.addEventListener("mouseover", (event) => {
-/*   console.log(event.target);
+  /*   console.log(event.target);
   console.log(event.currentTarget); */
   getShiftImage();
 });
 
 banner.addEventListener("mouseout", (event) => {
   getOriginalImage();
+});
+
+slider.addEventListener("mousedown", (event) => {
+  isDragging = true;
+  event.currentTarget.style.cursor = "grabbing";
+});
+
+document.addEventListener("mousemove", (event) => {
+  if (isDragging) {
+    const bannerPosition = banner.getBoundingClientRect();
+    const sliderWidth = slider.offsetWidth;
+
+    let newLeft = event.clientX - bannerPosition.left - sliderWidth / 2;
+
+    if (newLeft < -sliderWidth / 2) newLeft = -sliderWidth / 2;
+    if (newLeft > bannerPosition.width - sliderWidth + sliderWidth / 2)
+      newLeft = bannerPosition.width - sliderWidth + sliderWidth / 2;
+
+    sliderPositionPercentage = (newLeft / bannerPosition.width) * 100;
+
+    slider.style.left = `${sliderPositionPercentage}%`;
+  }
+});
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+  slider.style.cursor = "pointer";
+});
+
+// Обновление позиции слайдера при изменении размеров окна
+window.addEventListener("resize", () => {
+  const bannerPosition = banner.getBoundingClientRect();
+
+  // Пересчитываем позицию слайдера в пикселях на основе процента
+  const newLeft = (sliderPositionPercentage / 100) * bannerPosition.width;
+  slider.style.left = `${newLeft}px`;
 });
