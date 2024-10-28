ef1a7a7d34b869a89317c8f3e46e60a27730946e
Qidia <qidian_n@mail.ru>
2024-10-28 17:09:48 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,67 +1,69 @@
-const banner = document.querySelector(".banner");
-const slider = document.querySelector(".slider");
+document.addEventListener("DOMContentLoaded", () => {
+  const banner = document.querySelector(".banner");
+  const slider = document.querySelector(".slider");
 
-const imgMountains = document.querySelector(".imgMountains");
-const shiftImageMountains = document.querySelector(".shiftImageMountains");
-const shiftBannerMountains = document.querySelector(".shiftBannerMountains");
+  const imgMountains = document.querySelector(".imgMountains");
+  const shiftImageMountains = document.querySelector(".shiftImageMountains");
+  const shiftBannerMountains = document.querySelector(".shiftBannerMountains");
 
-let isDragging = false; // Переменная состояния для отслеживания активности перетаскивания
-let sliderPositionPercentage = 32; // Начальная позиция слайдера в процентах
+  let isDragging = false; // Переменная состояния для отслеживания активности перетаскивания
+  let sliderPositionPercentage = 32; // Начальная позиция слайдера в процентах
 
-/**
- * Функция для обновления положения слайдера и установки значений clip-path в зависимости от текущей позиции слайдера.
- */
-function updateClipPaths() {
-  const bannerPosition = banner.getBoundingClientRect(); // Получаем размеры и положение баннера
-  const sliderWidth = slider.offsetWidth; // Получаем ширину слайдера
+  /**
+   * Функция для обновления положения слайдера и установки значений clip-path в зависимости от текущей позиции слайдера.
+   */
+  function updateClipPaths() {
+    const bannerPosition = banner.getBoundingClientRect(); // Получаем размеры и положение баннера
+    const sliderWidth = slider.offsetWidth; // Получаем ширину слайдера
 
-  // Расчет смещения от центра слайдера в процентах относительно ширины баннера
-  const offsetPercentage = ((sliderWidth / 2) * 100) / bannerPosition.width;
-  slider.style.left = `${sliderPositionPercentage}%`;
+    // Расчет смещения от центра слайдера в процентах относительно ширины баннера
+    const offsetPercentage = ((sliderWidth / 2) * 100) / bannerPosition.width;
+    slider.style.left = `${sliderPositionPercentage}%`;
 
-  // Устанавливаем значения clip-path для изображений в зависимости от положения слайдера
-  imgMountains.style.clipPath = `inset(0 ${
-    100 - sliderPositionPercentage - offsetPercentage
-  }% 0 0)`;
-  shiftImageMountains.style.clipPath = `inset(0 0 0 ${
-    sliderPositionPercentage + offsetPercentage
-  }%)`;
-  shiftBannerMountains.style.clipPath = `inset(0 0 0 ${
-    sliderPositionPercentage + offsetPercentage
-  }%)`;
-}
-
-// Включение перетаскивания при нажатии мыши на слайдере
-slider.addEventListener("mousedown", (event) => {
-  isDragging = true;
-  event.currentTarget.style.cursor = "grabbing";
-});
-
-// Обработчик для перемещения слайдера при движении мыши
-document.addEventListener("mousemove", (event) => {
-  if (isDragging) {
-    const bannerPosition = banner.getBoundingClientRect(); // Обновляем размеры баннера
-    const sliderWidth = slider.offsetWidth; // Обновляем ширину слайдера
-
-    let newLeft = event.clientX - bannerPosition.left - sliderWidth / 2; // Рассчитываем новую позицию слайдера относительно баннера
-    // Ограничиваем положение слайдера внутри границ баннера
-    newLeft = Math.max(
-      -sliderWidth / 2,
-      Math.min(newLeft, bannerPosition.width - sliderWidth / 2)
-    );
-    sliderPositionPercentage = (newLeft / bannerPosition.width) * 100; // Конвертируем новую позицию в проценты
-
-    requestAnimationFrame(updateClipPaths); // Снижаем нагрузку на браузер (плавные обновления clip-path и позиции)
+    // Устанавливаем значения clip-path для изображений в зависимости от положения слайдера
+    imgMountains.style.clipPath = `inset(0 ${
+      100 - sliderPositionPercentage - offsetPercentage
+    }% 0 0)`;
+    shiftImageMountains.style.clipPath = `inset(0 0 0 ${
+      sliderPositionPercentage + offsetPercentage
+    }%)`;
+    shiftBannerMountains.style.clipPath = `inset(0 0 0 ${
+      sliderPositionPercentage + offsetPercentage
+    }%)`;
   }
-});
 
-// Завершение перетаскивания при отпускании мыши
-document.addEventListener("mouseup", () => {
-  if (isDragging) {
-    isDragging = false;
-    slider.style.cursor = "pointer";
-  }
-});
+  // Включение перетаскивания при нажатии мыши на слайдере
+  slider.addEventListener("mousedown", (event) => {
+    isDragging = true;
+    event.currentTarget.style.cursor = "grabbing";
+  });
 
-window.addEventListener("resize", updateClipPaths); // Обновление позиции слайдера и clip-path при изменении размеров окна
-updateClipPaths(); // Инициализируем слайдер и clip-path при загрузке
+  // Обработчик для перемещения слайдера при движении мыши
+  document.addEventListener("mousemove", (event) => {
+    if (isDragging) {
+      const bannerPosition = banner.getBoundingClientRect(); // Обновляем размеры баннера
+      const sliderWidth = slider.offsetWidth; // Обновляем ширину слайдера
+
+      let newLeft = event.clientX - bannerPosition.left - sliderWidth / 2; // Рассчитываем новую позицию слайдера относительно баннера
+      // Ограничиваем положение слайдера внутри границ баннера
+      newLeft = Math.max(
+        -sliderWidth / 2,
+        Math.min(newLeft, bannerPosition.width - sliderWidth / 2)
+      );
+      sliderPositionPercentage = (newLeft / bannerPosition.width) * 100; // Конвертируем новую позицию в проценты
+
+      requestAnimationFrame(updateClipPaths); // Снижаем нагрузку на браузер (плавные обновления clip-path и позиции)
+    }
+  });
+
+  // Завершение перетаскивания при отпускании мыши
+  document.addEventListener("mouseup", () => {
+    if (isDragging) {
+      isDragging = false;
+      slider.style.cursor = "pointer";
+    }
+  });
+
+  window.addEventListener("resize", updateClipPaths); // Обновление позиции слайдера и clip-path при изменении размеров окна
+  updateClipPaths(); // Инициализируем слайдер и clip-path при загрузке
+});
